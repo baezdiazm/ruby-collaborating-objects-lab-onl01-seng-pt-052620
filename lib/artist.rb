6c425866d39e828a18633790cea1a8cde3dedc9e
Manuel Eduardo Baez Diaz <baezdiazm@yahoo.com>
2020-07-17 18:11:43 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,29 +1,29 @@
 class Artist
-  attr_accessor :name
-  @@all = []
-  @@songs = []
-  @@song_count = 0
 
-  def initialize(name)
+  attr_accessor :name, :songs
+
+  @@all = [] #array of artists instances
+
+  def initialize(name)#artist_name
     @name = name
-    @@all << self
+    @songs = [] #array of songs for each instance
+  end
+
+  #this exposes the @@all Class Variable
+  def self.all
+    @@all
   end
 
   def add_song(song)
-    @@songs << song
-    song.artist = self
-  end
-
-  def self.find(name)
-   @@all.find do |artist|
-     artist.name == name
-   end
-  end
-
-  def create(name)
-    artist = self.new(name)
-    @@all << artist
-    artist
+    #called by the song instance, adds in that song instance
+    #called inside the song method #artist_name=
+    #after the song has been created, assigned a name
+    #the song class checks to see if there is an artist associated with the song, and if there isnt' one, it asks the Artist class to create an artist instance
+    #it then passes that artist instance back to the song to store as an attribute
+    #the song class then calls this method #add_song
+    #this method adds the song to the artist instances song array
+    #every artist instance has this array
+    @songs << song
   end
 
   def self.find_or_create_by_name(name)
@@ -34,15 +34,29 @@ class Artist
     end
   end
 
-  def self.all
-    @@all
+
+  def self.find(name)
+    @@all.find do |artist|
+      artist.name == name
+    end
   end
 
-  def songs
-    @@songs
+  def self.create(name)
+    artist = self.new(name)
+    @@all << artist
+    artist
   end
 
-  def print_songs
-    puts self.songs
+
+
+  def save
+    @@all << self
   end
+
+  #this method prints out an array of song names
+  #by accessing the name attribute of each song
+  def print_songs()
+    puts @songs.collect {|x| x.name}
+  end
+
 end
